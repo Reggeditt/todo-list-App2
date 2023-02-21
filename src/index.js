26be1b8e8fc362f4e94107783eab94181942f64e
Reggeditt <tkodjosarso@gmail.com>
2023-02-21 18:46:41 +0000

    create tasklist array to render dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './index.css';
+
+const todoListTasks = [
+  {
+    description: 'Wash the dishes',
+    isCompleted: false,
+    taskIndex: null,
+  },
+  {
+    description: 'clear the room',
+    isCompleted: false,
+    taskIndex: null,
+  },
+  {
+    description: 'buy groceries',
+    isCompleted: false,
+    taskIndex: null,
+  },
+];
+
+const todoListWrapperElement = document.getElementById('todo-list-wrap');
+todoListTasks.forEach((task, i) => {
+  task.taskIndex = i;
+  const newListTag = document.createElement('li');
+  newListTag.textContent = task.description;
+  newListTag.className = 'todo-list-item';
+  todoListWrapperElement.appendChild(newListTag);
+});
