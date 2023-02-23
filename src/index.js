403002247219686ef48996d603fb6e561f897939
Reggeditt <tkodjosarso@gmail.com>
2023-02-23 03:50:53 +0000

    add completed toggle interaction

--- src/index.js
+++ src/index.js
@@ -24,18 +24,71 @@ class TodoListData {
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.id = `checkbox${task.taskIndex}`;
+      checkbox.dataset.id = task.taskIndex;
       const listDescription = document.createElement('input');
       listDescription.type = 'text';
+      listDescription.className = 'todo-list-item-description';
       listDescription.value = task.description;
+      const taskRemoveBtn = document.createElement('button');
+      taskRemoveBtn.className = 'todo-list-item-remove';
+      taskRemoveBtn.dataset.id = task.taskIndex;
       const newListTag = document.createElement('li');
       newListTag.className = 'todo-list-item';
       newListTag.id = `list-item${task.taskIndex}`;
+
+      // implement drag and drop functionality
       newListTag.draggable = true;
       newListTag.addEventListener('dragstart', drag);
       newListTag.addEventListener('drop', drop);
       newListTag.addEventListener('dragover', allowDrop);
-      newListTag.append(checkbox, listDescription);
+      newListTag.append(checkbox, listDescription, taskRemoveBtn);
       todoListWrapperElement.appendChild(newListTag);
+    });
+
+    // toggle isCompleted to true/false when checkbox is checked/unchecked
+    const todoListWrapperChildren = Array.from(todoListWrapperElement.children);
+    todoListWrapperChildren.forEach((child, index) => {
+      child.children[0].addEventListener('click', () => {
+        if (child.children[0].checked) {
+          child.children[1].style.textDecoration = 'line-through';
+          this.todoListTasks[index].isCompleted = true;
+        } else {
+          child.children[1].style.textDecoration = 'none';
+          this.todoListTasks[index].isCompleted = false;
+        }
+      });
+    });
+
+    // add event listener to input field to toggle styling and update task description
+    const inputFields = Array.from(document.getElementsByClassName('todo-list-item-description'));
+    inputFields.forEach((input, i) => {
+      input.addEventListener('focus', () => {
+        todoListWrapperElement.children[i].style.backgroundColor = 'yellow';
+        todoListWrapperElement.children[i].children[2].style.display = 'block';
+        input.style.backgroundColor = 'yellow';
+      });
+      input.addEventListener('focusout', () => {
+        todoListWrapperElement.children[i].style.backgroundColor = 'white';
+        todoListWrapperElement.children[i].children[2].style.display = 'none';
+        input.style.backgroundColor = 'white';
+      });
+      input.addEventListener('change', () => {
+        this.todoListTasks[i].description = input.value;
+        todoListWrapperElement.children[i].style.backgroundColor = 'white';
+        todoListWrapperElement.children[i].children[2].style.display = 'none';
+        input.style.backgroundColor = 'white';
+        input.focus = false;
+      });
+    });
+
+    // add event listener to delete button to remove task from the list
+    const removeBtns = Array.from(document.getElementsByClassName('todo-list-item-remove'));
+    removeBtns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const taskIndex = btn.dataset.id;
+        console.log('taskIndex: ', taskIndex);
+        this.removeTask(taskIndex);
+      });
     });
   }
 }
